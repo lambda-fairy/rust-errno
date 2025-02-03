50441d9ae7d430ddd47a8ccf129f697579381e72
Kartik Agarwala <agarwala.kartik@gmail.com>
2025-02-03 11:56:16 +0530

    Add vxworks platform support

--- src/unix.rs
+++ src/unix.rs
@@ -77,6 +77,7 @@ extern "C" {
             target_os = "netbsd",
             target_os = "android",
             target_os = "espidf",
+            target_os = "vxworks",
             target_env = "newlib"
         ),
         link_name = "__errno"
